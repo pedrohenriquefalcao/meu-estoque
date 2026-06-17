03b0dad14ebd1c14957a6c9158f71397d1641182
pedrodamassa701 <pedrocostafalcao7@gmail.com>
2026-06-16 23:03:38 -0300

    salvar aqui

--- src/com/meuestoque/repository/ConexaoDatabase.java
+++ src/com/meuestoque/repository/ConexaoDatabase.java
@@ -1,6 +1,14 @@
 package com.meuestoque.repository;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexaoDatabase {
+
+    private static final String URL = "";
+    private static final String USUARIO = "postgres";
+    private static final String SENHA = "";
 
 
 
