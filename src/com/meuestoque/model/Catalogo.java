15702c0e4fdfcc9d3c722b2de1854c605d35d766
pedrodamassa701 <pedrocostafalcao7@gmail.com>
2026-06-16 22:42:30 -0300

    feat: classe Catalogo e Produto no pct model

--- src/com/meuestoque/model/Catalogo.java
+++ src/com/meuestoque/model/Catalogo.java
@@ -1,4 +1,51 @@
 package com.meuestoque.model;
 
 public class Catalogo {
+    private int idCatalogo;
+    private String nomeProduto;
+    private String descricao;
+    private double precoBase;
+
+    public Catalogo() {
+    }
+
+    public Catalogo(int idCatalogo, String nomeProduto, String descricao, double precoBase) {
+        this.idCatalogo = idCatalogo;
+        this.nomeProduto = nomeProduto;
+        this.descricao = descricao;
+        this.precoBase = precoBase;
+
+    }
+
+    public int getIdCatalogo() {
+        return idCatalogo;
+    }
+
+    public void setIdCatalogo(int idCatalogo) {
+        this.idCatalogo = idCatalogo;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPrecoBase() {
+        return precoBase;
+    }
+
+    public void setPrecoBase(double precoBase) {
+        this.precoBase = precoBase;
+    }
 }
--- src/com/meuestoque/model/Produto.java
+++ src/com/meuestoque/model/Produto.java
@@ -1,4 +1,61 @@
 package com.meuestoque.model;
 
 public class Produto {
+    private int idProduto;
+    private int idCatalogo;
+    private int idSabor;
+    private int idLote;
+    private double precoVenda;
+
+public Produto() {
 }
+
+public Produto (int idProduto, int idCatalogo, int idSabor, int idLote, double precoVenda) {
+    this.idProduto = idProduto;
+    this.idCatalogo = idCatalogo;
+    this.idSabor = idSabor;
+    this.idLote = idLote;
+    this.precoVenda = precoVenda;
+}
+
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public int getIdCatalogo() {
+        return idCatalogo;
+    }
+
+    public void setIdCatalogo(int idCatalogo) {
+        this.idCatalogo = idCatalogo;
+    }
+
+    public int getIdSabor() {
+        return idSabor;
+    }
+
+    public void setIdSabor(int idSabor) {
+        this.idSabor = idSabor;
+    }
+
+    public int getIdLote() {
+        return idLote;
+    }
+
+    public void setIdLote(int idLote) {
+        this.idLote = idLote;
+    }
+
+    public double getPrecoVenda() {
+        return precoVenda;
+    }
+
+    public void setPrecoVenda(double precoVenda) {
+        this.precoVenda = precoVenda;
+    }
+}
+
