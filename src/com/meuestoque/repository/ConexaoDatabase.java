5a03bc3f6c26757294537b591b1383c5013b18fe
pedrodamassa701 <pedrocostafalcao7@gmail.com>
2026-06-16 23:52:07 -0300

    feat: conexão com o BD pronta e online agora falta fazer o resto kkkkk até amanhã

--- src/com/meuestoque/repository/ConexaoDatabase.java
+++ src/com/meuestoque/repository/ConexaoDatabase.java
@@ -11,4 +11,20 @@ public class ConexaoDatabase {
     private static final String SENHA = System.getenv("DB_PASSWORD");
 
 
+    public static Connection conectar() {
+        try {
+            Class.forName("org.postgresql.Driver");
+
+            return DriverManager.getConnection(URL, USUARIO, SENHA);
+        } catch (ClassNotFoundException e) {
+            System.err.println("Erro: Driver JDBC do PostgreSQL não foi encontrado!");
+            e.printStackTrace();
+            return null;
+        } catch (SQLException e) {
+            System.err.println("Erro: Falha na conexão com o banco! Verifique as suas credenciais.");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
