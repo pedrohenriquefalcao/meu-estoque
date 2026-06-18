b8c50084f0e81c9bdcd4e71bda7a5abc6b3e71d8
pedrodamassa701 <pedrocostafalcao7@gmail.com>
2026-06-17 22:16:23 -0300

    feat: SET do SaborRepository.java

---
+++ src/com/meuestoque/repository/SaborRepository.java
@@ -0,0 +1,36 @@
+package com.meuestoque.repository;
+
+import com.meuestoque.model.Sabor;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SaborRepository {
+
+    public void salvar(Sabor sabor) {
+        String sql = "INSERT INTO sabor (nome_sabor) VALUES (?)";
+
+        try (Connection conn = ConexaoDatabase.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, sabor.getNomeSabor());
+            stmt.executeUpdate();
+
+            System.out.println("Sabor '"+sabor.getNomeSabor()+"' inserido com sucesso!");
+
+        } catch (SQLException e) {
+            System.err.println("Erro ao salvar sabor!");
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+
+
+}
