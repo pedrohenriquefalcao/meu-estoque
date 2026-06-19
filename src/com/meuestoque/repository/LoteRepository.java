29f423e06a6404cf5a524d293c9fb6fb505e846a
pedrodamassa701 <pedrocostafalcao7@gmail.com>
2026-06-18 22:25:40 -0300

    feat: REPO > lote e produto

--- src/com/meuestoque/repository/LoteRepository.java
+++ src/com/meuestoque/repository/LoteRepository.java
@@ -1,4 +1,28 @@
 package com.meuestoque.repository;
 
+import com.meuestoque.model.Lote;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Date;
+
 public class LoteRepository {
+
+    public void salvar(Lote lote) {
+        String sql = "INSERT INTO lote (data_feito, data_prazo) VALUES (?, ?)";
+
+        try (Connection conn = ConexaoDatabase.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setDate(1, Date.valueOf(lote.getDataFeito()));
+            stmt.setDate(2, Date.valueOf(lote.getDataPrazo()));
+
+            stmt.executeUpdate();
+            System.out.println("Sucesso!! Lote registrado no estoque.");
+
+        } catch (SQLException e) {
+            System.err.println("Erro ao salvar o lote no banco de dados!");
+            e.printStackTrace();
+        }
+    }
 }
--- src/com/meuestoque/repository/ProdutoRepository.java
+++ src/com/meuestoque/repository/ProdutoRepository.java
@@ -1,4 +1,30 @@
 package com.meuestoque.repository;
 
+import com.meuestoque.model.Produto;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class ProdutoRepository {
+
+    public void salvar(Produto produto) {
+        String sql = "INSERT INTO produto (id_catalogo, id_sabor, id_lote, preco_venda) VALUES (?, ?, ?, ?)";
+
+        try (Connection conn = ConexaoDatabase.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, produto.getCatalogo().getIdCatalogo());
+            stmt.setInt(2, produto.getSabor().getIdSabor());
+            stmt.setInt(3, produto.getLote().getIdLote());
+
+            stmt.setBigDecimal(4, produto.getPrecoVenda());
+
+            stmt.executeUpdate();
+            System.out.println("Sucesso!! Novo produto montado e pronto para venda.");
+
+        } catch (SQLException e) {
+            System.err.println("Erro ao cadastrar o produto no estoque!");
+            e.printStackTrace();
+        }
+    }
 }
